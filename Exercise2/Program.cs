8dd98c14773e99e1ff212c7d90b1d078e8a43c59
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2022-10-31 16:47:34 +0700

    membuat method algoritma merge sort

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -60,11 +60,79 @@ namespace Exercise2
             Console.WriteLine("-----------------------------------");
             Console.WriteLine(" Element array yang telah tersusun ");
             Console.WriteLine("-----------------------------------");
-            for (int j = 0; j < n; j++)
+            for (int AW= 0; AW < n; AW++)
             {
-                Console.WriteLine(a[j]);
+                Console.WriteLine(aan[AW]);
             }
             Console.WriteLine("");
+        }
+        void merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            
+            for (i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+
+            i = 0;
+            j = 0;
+
+
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        void sort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+
+                int m = l + (r - l) / 2;
+
+
+                sort(arr, l, m);
+                sort(arr, m + 1, r);
+
+
+                merge(arr, l, m, r);
+            }
         }
         static void Main(string[] args)
         {
@@ -90,7 +158,7 @@ namespace Exercise2
                         Console.WriteLine(". . . . . . . . . . . .");
                         P.input();
                         P.insertion_sort();
-
+                        P.display();
                         break;
                     case 2:
                         Console.WriteLine("");
