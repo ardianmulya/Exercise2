716f2640b1b6d46c55ba645cfae6a522819d565f
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2022-10-31 15:40:58 +0700

    Add project files.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        private int[] aan = new int[39];
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
