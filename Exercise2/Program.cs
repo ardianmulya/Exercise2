867ab54071b4b51e21a134192a975390e21aa08e
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2022-10-31 16:39:45 +0700

    membuat method menampilkan array yang sudah urut

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -53,7 +53,19 @@ namespace Exercise2
             }
         }
 
-
+        public void display()
+        {
+            //Menampilkan array yang tersusun
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(" Element array yang telah tersusun ");
+            Console.WriteLine("-----------------------------------");
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+            Console.WriteLine("");
+        }
         static void Main(string[] args)
         {
             Program P = new Program();
